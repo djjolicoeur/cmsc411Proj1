ee1b6a23676063c0acf31edee55d21278e940546
Daniel Jolicoeur <djjolicoeur@gmail.com>
2015-04-15 03:26:07 -0400

    normalize to pi/radians

--- sine.s
+++ sine.s
@@ -4,6 +4,7 @@ fact:   .double 6.0
 flag:   .double -1.0
 negone: .double -1.0
 two:    .double 2.0
+pi:     .double 3.14159265359
 
         .text
 
@@ -12,7 +13,6 @@ two:    .double 2.0
 main:
         li $v0,7
         syscall
-        mov.d $f2,$f0
         li $v0,5
         syscall
         add $t0,$v0,$zero
@@ -22,7 +22,10 @@ main:
         l.d $f8,flag
         l.d $f10,negone
         l.d $f18,two
-        #Seed series
+        l.d $f20, pi
+        c.le.d $f0, $f20
+        bc1f normalize
+        mov.d $f2,$f0
         mul.d $f12,$f2,$f2
         mul.d $f12,$f12,$f2
         div.d $f14,$f12,$f6
@@ -48,6 +51,28 @@ step:
         add.d $f4,$f4,$f18
         addi $t1,$t1,1
         b step
+
+
+normalize:
+        mul.d $f22,$f20,$f18
+        mul.d $f24,$f20,$f0
+        add.d $f26,$f0,$f20
+        div.d $f26,$f26,$f22
+        floor.w.d  $f26,$f26
+        cvt.d.w  $f26,$f26
+        sub.d $f28,$f0,$f22
+        mul.d $f0,$f26,$f28
+        mov.d $f12,$f0
+        mov.d $f2,$f0
+        mul.d $f12,$f2,$f2
+        mul.d $f12,$f12,$f2
+        div.d $f14,$f12,$f6
+        mul.d $f14,$f14,$f8
+        mul.d $f8,$f8,$f10
+        add.d $f2,$f2,$f14
+        add.d $f4,$f4,$f18
+
+
 
 
 jump:
